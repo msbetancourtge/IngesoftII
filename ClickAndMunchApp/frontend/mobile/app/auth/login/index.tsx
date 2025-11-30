1f5f0c2cfc485f9aabeecd60a73755b5d9c5b6c5
Cristhian Cely <yamitosu@gmail.com>
2025-11-30 15:36:27 -0500

    Login Screen finished

--- ClickAndMunchApp/frontend/mobile/app/auth/login/index.tsx
+++ ClickAndMunchApp/frontend/mobile/app/auth/login/index.tsx
@@ -2,49 +2,69 @@ import { View, Text, KeyboardAvoidingView, ScrollView, useWindowDimensions, Text
 import React from 'react'
 import { ThemedText } from '@/presentation/theme/components/themed-text'
 import ThemedTextInput from '@/presentation/theme/components/themed-text-input';
+import ThemedButton from '@/presentation/theme/components/themed-button';
+import ThemedLink from '@/presentation/theme/components/themed-link';
 
 const LoginScreen = () => {
 
   const { height } = useWindowDimensions();
 
-  return (
+    return (
 
-    <KeyboardAvoidingView behavior='padding' style={{ flex: 1 }}>
-      <ScrollView
-        style={{
-          paddingHorizontal: 40,
-        }}
-      >
-        <View
+      <KeyboardAvoidingView behavior='padding' style={{ flex: 1 }}>
+        <ScrollView
           style={{
-            paddingTop: height * 0.35,
+            paddingHorizontal: 40,
           }}
         >
-          <ThemedText type='title'>Ingresar</ThemedText>
-          <ThemedText style={{ color: 'grey', paddingTop: 10, paddingBottom: 20 }}>Por favor ingrese para continuar</ThemedText>
-        </View>
+          <View
+            style={{
+              paddingTop: height * 0.35,
+            }}
+          >
+            <ThemedText type='title'>Ingresar</ThemedText>
+            <ThemedText style={{ color: 'grey', paddingTop: 10, paddingBottom: 20 }}>Por favor ingrese para continuar</ThemedText>
+          </View>
 
-        
-        <View>
-          <ThemedTextInput
-            placeholder='Correo electronico'
-            keyboardType='email-address'
-            autoCapitalize='none'
-            icon='mail-outline'
-          />
           
-          <ThemedTextInput
-            placeholder='Contraseña'
-            secureTextEntry
-            autoCapitalize='none'
-            icon='lock-closed-outline'
-          />
-        </View>
+          <View>
+            <ThemedTextInput
+              placeholder='Correo electronico'
+              keyboardType='email-address'
+              autoCapitalize='none'
+              icon='mail-outline'
+            />
+            
+            <ThemedTextInput
+              placeholder='Contraseña'
+              secureTextEntry
+              autoCapitalize='none'
+              icon='lock-closed-outline'
+            />
+          </View>
 
-      </ScrollView>
+          <View style={{ marginTop: 10 }} />
 
-    </KeyboardAvoidingView>
-  )
+          <ThemedButton>Ingresar</ThemedButton>
+
+          <View style={{ marginTop: 50 }} />
+
+          <View style={{
+            flexDirection: 'row',
+            justifyContent: 'center',
+            alignItems: 'center'
+          }}>
+              <ThemedText style={{marginHorizontal: 10}}>No tienes cuenta?</ThemedText>
+              <ThemedLink href='/auth/register' >
+                Crear cuenta
+              </ThemedLink>
+
+          </View>
+
+        </ScrollView>
+
+      </KeyboardAvoidingView>
+    )
 }
 
 export default LoginScreen
