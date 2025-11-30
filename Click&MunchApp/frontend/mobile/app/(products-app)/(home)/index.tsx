7ff83b4105e224b89f8d3bcf06db780b30e35560
Cristhian Cely <yamitosu@gmail.com>
2025-11-30 11:17:14 -0500

    Creation mobile project

---
+++ Click&MunchApp/frontend/mobile/app/(products-app)/(home)/index.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { ThemedText } from '@/components/themed-text'
+
+const HomeScreen = () => {
+  return (
+    <View>
+      <ThemedText>HomeScreen</ThemedText>
+    </View>
+  )
+}
+
+export default HomeScreen
