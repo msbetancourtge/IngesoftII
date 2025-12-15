f5b6ee3dd55ff9634bce448bfd1cb5d0b0cef7ea
Mike <msbetancourtge@unal.edu.co>
2025-12-14 21:17:39 -0500

    terraform dependencies modified

--- infra/terraform/modules/network/main.tf
+++ infra/terraform/modules/network/main.tf
@@ -1,5 +1,9 @@
 variable "vpc_cidr" { type = string }
 
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
 resource "aws_vpc" "main" {
   cidr_block           = var.vpc_cidr
   enable_dns_support   = true
@@ -9,21 +13,25 @@ resource "aws_vpc" "main" {
 resource "aws_subnet" "public_a" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = cidrsubnet(var.vpc_cidr, 4, 0)
+  availability_zone       = data.aws_availability_zones.available.names[0]
   map_public_ip_on_launch = true
 }
 resource "aws_subnet" "public_b" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = cidrsubnet(var.vpc_cidr, 4, 1)
+  availability_zone       = data.aws_availability_zones.available.names[1]
   map_public_ip_on_launch = true
 }
 
 resource "aws_subnet" "private_a" {
   vpc_id     = aws_vpc.main.id
   cidr_block = cidrsubnet(var.vpc_cidr, 4, 2)
+  availability_zone = data.aws_availability_zones.available.names[0]
 }
 resource "aws_subnet" "private_b" {
   vpc_id     = aws_vpc.main.id
   cidr_block = cidrsubnet(var.vpc_cidr, 4, 3)
+  availability_zone = data.aws_availability_zones.available.names[1]
 }
 
 resource "aws_internet_gateway" "igw" {
